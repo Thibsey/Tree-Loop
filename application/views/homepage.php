b4b102bc4a2d580aa36e4d549cf7a4729700442e
talar90 <tito100190@hotmail.com>
2018-03-04 11:48:49 +0100

    add new branch with homepage

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,17 +1,191 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Home Page</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-    <h1>We are inside Home Page</h1>
-   
-    <a href="/register"><button>Register</button></a> 
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Lesser &mdash; Free HTML5 Bootstrap Website Template by FreeHTML5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FreeHTML5.co" />
 
-</body>
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700,900' rel='stylesheet' type='text/css'>
+
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/simple-line-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+    <style>
+
+body {
+  font-family: "Roboto", Arial, sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.7;
+  color: #7f7f7f;
+  background: #fff;
+  height: 100%;
+  position: relative;
+  text-align:center;
+}
+a {
+  color: #1FB57B;
+  -webkit-transition: 0.5s;
+  -o-transition: 0.5s;
+  transition: 0.5s;
+}
+a:hover, a:active, a:focus {
+  color: #1FB57B;
+  outline: none;
+}
+p {
+  margin-bottom: 30px;
+  font-size: 18px;
+  font-weight: 400;
+}
+h1, h2, h3, h4, h5, h6, figure {
+  color: rgba(0, 0, 0, 0.7);
+  font-family: "Playfair Display", Arial, serif;
+  font-weight: 400;
+  margin: 0 0 30px 0;
+  letter-spacing: 1px;
+}
+#fh5co-services-section,
+#fh5co-blog-section,
+#fh5co-client-section,
+#fh5co-work-section,
+#fh5co-main-services-section,
+#fh5co-about-section,
+#fh5co-contact-section {
+  padding-top: 6em;
+}
+#fh5co-intro-section {
+  padding-top: 6em;
+}
+.featured-grid, .featured-grid.featured-grid-2 {
+  width: 100%;
+  height: 510px;
+  float: left;
+  background-size: cover;
+  background-position: center center;
+  position: relative;
+  overflow: hidden;
+  -webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  -ms-border-radius: 4px;
+  border-radius: 4px;
+}
+#fh5co-footer {
+  padding: 7em 0;
+  float: left;
+  width: 100%;
+  position: relative;
+  background: #262626;
+}
+#fh5co-page {
+  position: relative;
+  z-index: 2;
+  background: #fff;
+}
+#fh5co-page {
+  z-index: 2;
+  -webkit-transition: 0.5s;
+  -o-transition: 0.5s;
+  transition: 0.5s;
+}
+.buttonclass{
+    padding-top:50px;
+}
+.firstpic{
+    background-image: url("https://i.imgur.com/WZW82Wm.jpg");
+    height:500px;
+}
+#idcenter{
+    text-align:center;
+    padding-top:80px;
+}
+
+    </style>
+	</head>
+	<body>	
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<img src="https://i.imgur.com/M6JQeoM.png">
+            </div class="buttonclass">
+            <div>
+            <button type="button" class="btn btn-outline-secondary">thuersday gathering</button>
+            <button type="button" class="btn btn-outline-secondary">international</button>
+            <button type="button" class="btn btn-outline-secondary">community</button>
+            <button type="button" class="btn btn-outline-secondary">about us</button>
+            <button type="button" class="btn btn-outline-secondary">contact us</button>
+			</div>
+		
+	</header>
+	<div id="fh5co-intro-section">
+		<div class="firstpic">
+			<div class="col-md-8 col-md-offset-2 text-center" id="idcenter">
+				<p>Connecting innovators to make things happen
+Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!
+</p>
+<a href="#">learn more</a>
+			</div>
+		</div>	
+	</div>
+    <div id="fh5co-intro-section">
+		<div>
+			<div class="col-md-8 col-md-offset-2 text-center">
+				<h2>Lesser is a creative agency that love to create functional website</h2>
+			</div>
+		</div>
+	</div>
+    <div id="fh5co-intro-section">
+		<div>
+			<div class="col-md-8 col-md-offset-2 text-center">
+				<h2>Lesser is a creative agency that love to create functional website</h2>
+			</div>
+        </div>        
+	</div>
+	</div>
+	
+	<footer id="fh5co-footer" role="contentinfo">
+	
+	
+	</footer>
+	</div>
+	
+	
+
+	</body>
 </html>
