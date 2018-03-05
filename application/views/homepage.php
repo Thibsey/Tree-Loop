8e75d4f9d3becb2c582db0f675b48581f8bec9a5
Thibs <tihantb@gmail.com>
2018-03-05 13:34:48 +0100

    #14 minor changes I forgot to commit but are important

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -142,7 +142,7 @@ defined('BASEPATH') or exit('No direct script access allowed');?>
 	<div >
 	<header>
 		<div  id="headerpic">
-			<img src="https://i.imgur.com/M6JQeoM.png">
+			<a href="http://localhost"><img src="https://i.imgur.com/M6JQeoM.png"></a>
     </div class="buttonclass">
     <div id="headernav">
             <button type="button" class="btn btn-outline-secondary">thuersday gathering</button>
@@ -177,23 +177,23 @@ defined('BASEPATH') or exit('No direct script access allowed');?>
 			</div>
 		</div>
 	</div>
-    <div id="fh5co-intro-section">
+    <div id="fh5co-intro-section" class="col-md-12">
 		<table>
         <tr>
         <td>
-			<div class="col-md-8 col-md-offset-2 text-center" id="mainpic">
+			<div class="col-md-10 col-md-offset-2 text-center" id="mainpic">
                 <img src="https://i.imgur.com/M6JQeoM.png" alt="" align="bottom">
                 <p>Connecting innovators to make things happen Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
 			</div>
             </td>
             <td>
-            <div class="col-md-8 col-md-offset-2 text-center" >
+            <div class="col-md-10 col-md-offset-2 text-center" >
                 <img src="https://i.imgur.com/M6JQeoM.png" alt="">
                 <p>Connecting innovators to make things happen Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
 			</div>
             </td>
             <td>
-            <div class="col-md-8 col-md-offset-2 text-center" >
+            <div class="col-md-10 col-md-offset-2 text-center" >
                 <img src="https://i.imgur.com/M6JQeoM.png" alt="">
                 <p>Connecting innovators to make things happen Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
 			</div>
