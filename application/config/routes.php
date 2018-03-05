b3d49a50645414ca518ef20a50ad5ac0671abbad
talar90 <tito100190@hotmail.com>
2018-03-05 09:59:43 +0100

    front end commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,8 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 $route['default_controller'] = 'process';
+$route['login'] = 'processes/login';
 $route['index'] = 'process/index';
 $route['register'] = 'process/register';
-$route['login'] = 'process/login';
+$route['join'] = 'process/bringmejoin';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
