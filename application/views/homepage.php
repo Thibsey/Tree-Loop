4f75698b6c8d0e52fa0fd2d6342ade87622e6234
Thibs <tihantb@gmail.com>
2018-03-05 11:17:42 +0100

    #12 Homepage reindented

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Lesser &mdash; Free HTML5 Bootstrap Website Template by FreeHTML5.co</title>
@@ -17,9 +17,9 @@ defined('BASEPATH') or exit('No direct script access allowed'); ?>
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700,900' rel='stylesheet' type='text/css'>
 
 	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	
 	
 	<!-- Bootstrap  -->
@@ -36,108 +36,108 @@ defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
     <style>
 
-body {
-  font-family: "Roboto", Arial, sans-serif;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 1.7;
-  color: #7f7f7f;
-  background: #fff;
-  height: 100%;
-  position: relative;
-  text-align:center;
-}
-a {
-  color: #1FB57B;
-  -webkit-transition: 0.5s;
-  -o-transition: 0.5s;
-  transition: 0.5s;
-}
+        body {
+        font-family: "Roboto", Arial, sans-serif;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 1.7;
+        color: #7f7f7f;
+        background: #fff;
+        height: 100%;
+        position: relative;
+        text-align:center;
+        }
+        a {
+        color: #1FB57B;
+        -webkit-transition: 0.5s;
+        -o-transition: 0.5s;
+        transition: 0.5s;
+        }
 
-p {
-  margin-bottom: 30px;
-  font-size: 18px;
-  font-weight: 400;
-}
-h1, h2, h3, h4, h5, h6, figure {
-  color: rgba(0, 0, 0, 0.7);
-  font-family: "Playfair Display", Arial, serif;
-  font-weight: 400;
-  margin: 0 0 30px 0;
-  letter-spacing: 1px;
-}
-#fh5co-services-section,
-#fh5co-blog-section,
-#fh5co-client-section,
-#fh5co-work-section,
-#fh5co-main-services-section,
-#fh5co-about-section,
-#fh5co-contact-section {
-  padding-top: 6em;
-}
-#fh5co-intro-section {
-  padding-top: 6em;
-}
-.featured-grid, .featured-grid.featured-grid-2 {
-  width: 100%;
-  height: 510px;
-  float: left;
-  background-size: cover;
-  background-position: center center;
-  position: relative;
-  overflow: hidden;
-  -webkit-border-radius: 4px;
-  -moz-border-radius: 4px;
-  -ms-border-radius: 4px;
-  border-radius: 4px;
-}
-#fh5co-footer {
-  padding: 7em 0;
-  float: left;
-  width: 100%;
-  position: relative;
-  background: #262626;
-}
-#fh5co-page {
-  position: relative;
-  z-index: 2;
-  background: #fff;
-}
-#fh5co-page {
-  z-index: 2;
-  -webkit-transition: 0.5s;
-  -o-transition: 0.5s;
-  transition: 0.5s;
-}
-.buttonclass{
-    padding-top:50px;
-}
-.firstpic{
-    background-image: url("https://i.imgur.com/WZW82Wm.jpg");
-    height:500px;
-}
-#idcenter{
-    text-align:center;
-    padding-top:80px;
-}
-#headernav {
-    display:inline-block;
-    padding-top:0px;
-    padding-right:0px;
-}
-#headerpic{
-    display:inline-block;
-    padding-top:40px;
-    padding-right:350px;
-}
-#mainpic{
-   
-}
+        p {
+        margin-bottom: 30px;
+        font-size: 18px;
+        font-weight: 400;
+        }
+        h1, h2, h3, h4, h5, h6, figure {
+        color: rgba(0, 0, 0, 0.7);
+        font-family: "Playfair Display", Arial, serif;
+        font-weight: 400;
+        margin: 0 0 30px 0;
+        letter-spacing: 1px;
+        }
+        #fh5co-services-section,
+        #fh5co-blog-section,
+        #fh5co-client-section,
+        #fh5co-work-section,
+        #fh5co-main-services-section,
+        #fh5co-about-section,
+        #fh5co-contact-section {
+        padding-top: 6em;
+        }
+        #fh5co-intro-section {
+        padding-top: 6em;
+        }
+        .featured-grid, .featured-grid.featured-grid-2 {
+        width: 100%;
+        height: 510px;
+        float: left;
+        background-size: cover;
+        background-position: center center;
+        position: relative;
+        overflow: hidden;
+        -webkit-border-radius: 4px;
+        -moz-border-radius: 4px;
+        -ms-border-radius: 4px;
+        border-radius: 4px;
+        }
+        #fh5co-footer {
+        padding: 7em 0;
+        float: left;
+        width: 100%;
+        position: relative;
+        background: #262626;
+        }
+        #fh5co-page {
+        position: relative;
+        z-index: 2;
+        background: #fff;
+        }
+        #fh5co-page {
+        z-index: 2;
+        -webkit-transition: 0.5s;
+        -o-transition: 0.5s;
+        transition: 0.5s;
+        }
+        .buttonclass{
+            padding-top:50px;
+        }
+        .firstpic{
+            background-image: url("https://i.imgur.com/WZW82Wm.jpg");
+            height:500px;
+        }
+        #idcenter{
+            text-align:center;
+            padding-top:80px;
+        }
+        #headernav {
+            display:inline-block;
+            padding-top:0px;
+            padding-right:0px;
+        }
+        #headerpic{
+            display:inline-block;
+            padding-top:40px;
+            padding-right:350px;
+        }
+        #mainpic{
+        
+        }
 
 
     </style>
-	</head>
-	<body>	
+</head>
+<body>	
 	<div >
 	<header>
 		<div  id="headerpic">
@@ -157,11 +157,8 @@ h1, h2, h3, h4, h5, h6, figure {
 	<div id="fh5co-intro-section">
 		<div class="firstpic">
 			<div class="col-md-8 col-md-offset-2 text-center" id="idcenter">
-				<p>Connecting innovators to make things happen
-Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!
-Connecting innovators to make things happen
-Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
-<a href="#">learn more</a>
+				<p>Connecting innovators to make things happen Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community! Connecting innovators to make things happen Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
+                <a href="#">learn more</a>
 			</div>
 		</div>	
 	</div>
@@ -178,22 +175,19 @@ Venture Café Foundation builds innovation <br> communities. We host the largest
         <td>
 			<div class="col-md-8 col-md-offset-2 text-center" id="mainpic">
                 <img src="https://i.imgur.com/M6JQeoM.png" alt="" align="bottom">
-                <p>Connecting innovators to make things happen
-Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
+                <p>Connecting innovators to make things happen Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
 			</div>
             </td>
             <td>
             <div class="col-md-8 col-md-offset-2 text-center" >
                 <img src="https://i.imgur.com/M6JQeoM.png" alt="">
-                <p>Connecting innovators to make things happen
-Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
+                <p>Connecting innovators to make things happen Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
 			</div>
             </td>
             <td>
             <div class="col-md-8 col-md-offset-2 text-center" >
                 <img src="https://i.imgur.com/M6JQeoM.png" alt="">
-                <p>Connecting innovators to make things happen
-Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
+                <p>Connecting innovators to make things happen Venture Café Foundation builds innovation <br> communities. We host the largest weekly innovation community event in Rotterdam.<br> With the community, for the community!</p>
 			</div>
             </td>
             </tr>
@@ -209,5 +203,5 @@ Venture Café Foundation builds innovation <br> communities. We host the largest
 	
 	
 
-	</body>
+</body>
 </html>
